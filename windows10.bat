0ed322064c993178c22043101e411fee153199ae
idleglass <642338923@qq.com>
2019-03-30 21:11:18 +0800

    每200个文件执行一次tar命令，提高打包速度

--- windows10.bat
+++ windows10.bat
@@ -16,10 +16,39 @@ set "xxx=%xxx::=_%"
 set "xxx=%xxx:.=_%"
 set "xxx=%xxx: =_%"
 echo %xxx%
-for /F %%i in ('git diff  %1 %2  --name-only') do ( 
-	echo tar -rf %cd%\path_%xxx%.tar %%i
-	tar -rf %cd%\patch%xxx%.tar %%i
+
+rem md path_%xxx%
+
+echo git diff  %1 %2  --name-only
+
+setlocal enabledelayedexpansion
+set flag=0
+set files=
+for /F %%i in ('git diff  %1 %2  --name-only') do (
+	set /a flag+=1
+	set yyy=%%i
+	set "files=!files! !yyy!"
+	rem echo !files!
+
+	if !flag! equ 200 (
+		echo tar -r -f %cd%\path_%xxx%.tar !files!
+		tar -r -f %cd%\path_%xxx%.tar !files!
+		set /a flag=0
+		set files=
+	)
+	rem echo tar -r -f %cd%\path_%xxx%.tar %%i
+	rem tar -r -f %cd%\patch%xxx%.tar %%i
+	rem set yyy=%%i
+	rem set "yyy=!yyy:/=\!"
+	rem echo copy !yyy! path_%xxx%\!yyy!
+	rem copy !yyy! path_%xxx%\!yyy!
 )
+
+if !flag! gtr 0 (
+	echo tar -r -f %cd%\path_%xxx%.tar !files!
+	tar -r -f %cd%\path_%xxx%.tar !files!
+)
+
 echo SUCCESS!!!
 echo SUCCESS!!!
 echo SUCCESS!!!
